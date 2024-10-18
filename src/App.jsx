a38b2d6998ff5941fd12e4e0b385fed4795e14c3
syed zohaib <zohaibsyed914@gmail.com>
2024-10-18 22:41:57 +0500

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./App.css"
 
 function App() {
+  const [count, setCount] = useState(1)
+  const [secondCount, setSecondCount] = useState(count)
+
+
+  const minusHandler = () => {
+
+    setCount((num) => num > 1 ? num - 1 : num)
+  }
+  const plusHandler = () => {
+    setCount((num) => num + 1)
+  }
+
+  const secondMinusHandler = () => {
+    setSecondCount((num) => num > count ? num - count : num)
+  }
+
+  const secondPlusHandler = () => {
+    setSecondCount((num) => num + count)
+  }
+
+  const current = () => {
+    let date = new Date()
+    let local = date.toLocaleString()
+    return local;
+  }
+
+
+
   return (
     <>
 
       <div className="container">
         <div className="box">
           <div className="boxy1">
-            <button type="button" className='btn'>-</button>
-            <span >Step: 1</span>
-            <button type="button" className='btn'>+</button>
+            <button type="button" className='btn' onClick={minusHandler}>-</button>
+            <span >Step: {count}</span>
+            <button type="button" className='btn' onClick={plusHandler}>+</button>
           </div>
           <div className="boxy2">
-            <button type="button" className='btn'>-</button>
-            <span >Step: 1</span>
-            <button type="button" className='btn'>+</button>
+            <button type="button" className='btn' onClick={secondMinusHandler}>-</button>
+            <span >Step: {secondCount}</span>
+            <button type="button" className='btn' onClick={secondPlusHandler}>+</button>
           </div>
           <br />
           <div className="boxy3">
-            <p>Today is Friday Jun 21 2024</p>
+            <p>{current()}</p>
           </div>
         </div>
       </div>
@@ -31,3 +59,19 @@ function App() {
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
