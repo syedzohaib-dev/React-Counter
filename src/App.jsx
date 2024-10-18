27823036e3d0047af3ed0cb746f7c2c3cfc8e20a
syed zohaib <zohaibsyed914@gmail.com>
2024-10-18 17:13:15 +0500

    working

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import "./App.css"
+
+function App() {
+  return (
+    <>
+
+      <div className="container">
+        <div className="box">
+          <div className="boxy1">
+            <button type="button" className='btn'>-</button>
+            <span >Step: 1</span>
+            <button type="button" className='btn'>+</button>
+          </div>
+          <div className="boxy2">
+            <button type="button" className='btn'>-</button>
+            <span >Step: 1</span>
+            <button type="button" className='btn'>+</button>
+          </div>
+          <br />
+          <div className="boxy3">
+            <p>Today is Friday Jun 21 2024</p>
+          </div>
+        </div>
+      </div>
+
+
+
+    </>
+  )
+}
+
+export default App
